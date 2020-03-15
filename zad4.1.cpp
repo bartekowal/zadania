3a708e03ee59297840d5329caa1069d3377b7474
Bartosz Kowal <57255774+bartekowal@users.noreply.github.com>
2020-03-15 16:11:42 +0100

    Add files via upload

---
+++ zad4.1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class student
+{
+public:
+    student();
+    string Opis_ = "student grupy";
+    void printOpis();
+};
+
+class starosta : public student
+{
+public:
+    string Opis_ = "starosta grupy";
+    string starosta1;
+};
+
+student::student()
+{
+    cout << "Tworzenie obiektu klasy student o nazwie: " << Opis_ << endl;
+}
+
+void student::printOpis()
+{
+    cout << "Opis: " << Opis_ << endl;
+}
+
+int main()
+{
+    student stud;
+    starosta starosta1;
+    stud.printOpis();
+    starosta1.printOpis();
+
+}
---
+++ zad4.2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class student
+{
+public:
+    student();
+    string Opis_ = "student grupy";
+    void printOpis();
+};
+
+class starosta : public student
+{
+public:
+    starosta();
+    string Opis_ = "starosta grupy";
+    void printOpis();
+};
+
+student::student()
+{
+    cout << "Tworzenie obiektu klasy student o nazwie: " << Opis_ << endl;
+}
+
+void student::printOpis()
+{
+    cout << "Opis: " << Opis_ << endl;
+}
+starosta::starosta()
+{
+    cout << "Tworzenie obiektu klasy starosta o nazwie: " << Opis_ << endl;
+}
+void starosta::printOpis()
+{
+    cout << "Opis: " << Opis_ << endl;
+}
+
+int main()
+{
+    student stud;
+    starosta starosta1;
+    stud.printOpis();
+    starosta1.printOpis();
+
+}
