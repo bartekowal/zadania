aa749447b44d9680da70e7b3d3aa9ec3ab841f6e
Bartosz Kowal <57255774+bartekowal@users.noreply.github.com>
2020-03-29 20:07:59 +0200

    Update zad3.2.cc

--- zad3.2.cc
+++ zad3.2.cc
@@ -7,8 +7,8 @@ public:
     Vector(double);
     Vector(double, double);
     Vector(double, double, double);
-    void showElements();
-    void showElements(Vector);
+    void showvector(Vector);
+    void showvector();
     int dimension_;
 private:
     double data_[3] = { 0, 0, 0 };
@@ -38,34 +38,24 @@ Vector::Vector(double element1, double element2, double element3)
     data_[2] = element3;
 }
 
-void Vector::showElements()
-{
-    cout << " Zmienna typu Vector :" << endl;
-    for (int i = 0; i < dimension_; i++) {
-        cout << " wartosc elementu o indeksie " << i << "  : " << data_[0] << endl;
+void Vector::showvector(Vector vec){
+    for(int i = 0; i < vec.dimension_; i++){
+        cout << vec.data_[i] << endl;
     }
 }
 
-void Vector::showElements(Vector v)
-{
-    cout << " Zmienna typu Vector :" << endl;
-    for (int i = 0; i < v.dimension_; i++) {
-        cout << " wartosc elementu o indeksie " << i << "  : " << v.data_[0] << endl;
+void Vector::showvector(){
+    for(int i = 0; i < dimension_; i++){
+        cout << data_[i] << endl;
     }
 }
 
 int main()
 {
-    Vector r1(1.0); // R1
-    cout << " Obiekt klasy Vector o wymiarze:" << r1.dimension_ << endl;
-    cout << "Obiekt klasy Vector :" << r1.data_[0]  << endl;
-    Vector r2(2.0, 2.0); // R2
-    cout << " Obiekt klasy Vector o wymiarze:" << r2.dimension_ << endl;
-    Vector r3(3.0, 3.0, 3.0); // R2
-    cout << " Obiekt klasy Vector o wymiarze:" << r3.dimension_ << endl;
-
-    cout << "\n";
-    r2.showElements();
-    cout << "\n";
-    r2.showElements(r3);
+    Vector r2(1.0, 2.0); // R2
+    cout << "Obiekt klasy Vector o wymiarze:" << r2.dimension_ << endl;
+    Vector r3(1.0, 2.0, 3.0); // R3
+    cout << "Obiekt klasy Vector o wymiarze:" << r3.dimension_ << endl;
+    r2.showvector();
+    r2.showvector(r3);
 }
